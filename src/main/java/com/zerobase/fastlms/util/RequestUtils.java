5662b0002118d13d6ff2553f0203aaa6abc22743
Mike-Jeong <70117048+Mike-Jeong@users.noreply.github.com>
2022-07-15 23:45:11 +0900

    project init - 기존 프로젝트에서 몇가지 설정 변경 (maria db 에서 mysql db로 변경 및 smtp 설정 변경)

---
+++ src/main/java/com/zerobase/fastlms/util/RequestUtils.java
@@ -0,0 +1,74 @@
+package com.zerobase.fastlms.util;
+
+import javax.servlet.http.HttpServletRequest;
+
+public class RequestUtils {
+
+    public static String getUserAgent(HttpServletRequest request) {
+
+        String userAgent = null;
+
+        userAgent = request.getHeader("user-agent");
+
+        return userAgent;
+
+    }
+
+    public static String getClientIP(HttpServletRequest request) {
+
+        String ip = null;
+
+        ip = request.getRemoteAddr();
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("Proxy-Client-IP");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("WL-Proxy-Client-IP");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("HTTP_CLIENT_IP");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("HTTP_X_FORWARDED_FOR");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("X-Real-IP");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("X-RealIP");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getHeader("REMOTE_ADDR");
+
+        }
+
+        if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+
+            ip = request.getRemoteAddr();
+
+        }
+
+        return ip;
+
+    }
+}
